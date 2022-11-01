575c09d175d16598039003ae9fd458bfc0d72de4
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-11-01 15:22:16 +0100

    musicapp

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Artise, Song, Lyric
 # Register your models here.
+
+admin.site.register(Artise)
+admin.site.register(Song)
+admin.site.register(Lyric)
