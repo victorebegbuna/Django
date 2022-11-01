c5b2a7aca013b8d8a151db2f1fbb96d98306dda4
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-11-01 15:06:29 +0100

    musicapp

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,29 @@
+from ast import Pass
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Artise(models.Model):
+    first_name = models.CharField
+    last_name = models.CharField
+    age = models.IntegerField
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song(models.Model):
+    title = models.CharField
+    date_released = models.IntegerField
+    likes = models.CharField
+    artise_id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content = models.CharField
+    song_id = models.IntegerField
+
+
