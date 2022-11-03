90b6877aaa498d7e1349921bb851e62bc800ebc3
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-11-03 14:24:13 +0100

    musicapp

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -3,5 +3,5 @@ from .models import Artise, Song, Lyric
 # Register your models here.
 
 admin.site.register(Artise)
-admin.site.register(Song)
 admin.site.register(Lyric)
+admin.site.register(Song)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,10 +1,11 @@
-from unittest.util import _MAX_LENGTH
+from tkinter import CASCADE
 from django.db import models
+
 
 # Create your models here.
 class Artise(models.Model):
-    first_name:models.CharField
-    last_name:models.CharField
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
     def __str__(self):
@@ -12,16 +13,20 @@ class Artise(models.Model):
 
 
 class Song(models.Model):
-    title = models.CharField
+    artise_id = models.ForeignKey(Artise, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
     date_released = models.IntegerField
-    likes = models.CharField
-    artise_id = models.IntegerField()
+    likes = models.IntegerField()
+    
 
-    def __str__(self):
+    def __int__(self):
         return self.title
 
 class Lyric(models.Model):
-    content = models.CharField
-    song_id = models.IntegerField()
+    song_id = models.ForeignKey(Song, null=True, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+
+    def __int__(self):
+        return self.content
 
 
